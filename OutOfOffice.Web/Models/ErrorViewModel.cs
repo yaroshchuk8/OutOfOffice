18a8ad2f4e9503409d7a4490bac3bb49cd09afbe
yaroshchuk8 <vladonyaroshchuk@gmail.com>
2024-06-28 19:21:17 +0300

    Add project files.

---
+++ OutOfOffice.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OutOfOffice.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
