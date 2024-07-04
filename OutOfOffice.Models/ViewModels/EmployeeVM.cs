072da357b2cb70c1695d9a1a81ab0947017eee40
yaroshchuk8 <vladonyaroshchuk@gmail.com>
2024-07-04 12:08:16 +0300

    Add functionality to update employees

---
+++ OutOfOffice.Models/ViewModels/EmployeeVM.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OutOfOffice.Models.ViewModels
+{
+    // Creating dummy lists to populate fields in db
+    public class EmployeeVM
+    {
+        public EmployeeVM() 
+        {
+            SubdivisionList = new[] 
+            {
+                new SelectListItem
+                {
+                    Text = "Human Resources",
+                    Value = "Human Resources"
+                },
+                new SelectListItem
+                {
+                    Text = "IT",
+                    Value = "IT"
+                },
+                new SelectListItem
+                {
+                    Text = "Marketing",
+                    Value = "Marketing"
+                },
+                new SelectListItem
+                {
+                    Text = "Sales",
+                    Value = "Sales"
+                },
+                new SelectListItem
+                {
+                    Text = "Finance",
+                    Value = "Finance"
+                },
+                new SelectListItem
+                {
+                    Text = "Sales",
+                    Value = "Sales"
+                }
+            };
+
+            StatusList = new[]
+            {
+                new SelectListItem
+                {
+                    Text = "Active",
+                    Value = "Active"
+                },
+                new SelectListItem
+                {
+                    Text = "Inactive",
+                    Value = "Inactive"
+                }
+            };
+
+            PositionList = new[]
+            {
+                new SelectListItem
+                {
+                    Text = "Front-end developer",
+                    Value = "Front-end developer"
+                },
+                new SelectListItem
+                {
+                    Text = "Back-end developer",
+                    Value = "Back-end developer"
+                },
+                new SelectListItem
+                {
+                    Text = "Accountant",
+                    Value = "Accountant"
+                },
+                new SelectListItem
+                {
+                    Text = "Sales manager",
+                    Value = "Sales manager"
+                },
+                new SelectListItem
+                {
+                    Text = "Receptionist",
+                    Value = "Receptionist"
+                },
+                new SelectListItem
+                {
+                    Text = "Designer",
+                    Value = "Designer"
+                },
+                new SelectListItem
+                {
+                    Text = "Data analyst",
+                    Value = "Data analyst"
+                }
+            };
+        }
+
+        public Employee Employee { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> SubdivisionList { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> StatusList { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> PositionList { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<SelectListItem> HrManagerList { get; set; }
+    }
+}
--- OutOfOffice.Web/Controllers/EmployeeController.cs
+++ OutOfOffice.Web/Controllers/EmployeeController.cs
@@ -1,22 +1,103 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using OutOfOffice.DataAccess.Data;
 using OutOfOffice.Models;
+using OutOfOffice.Models.ViewModels;
 
 namespace OutOfOffice.Web.Controllers
 {
     public class EmployeeController : Controller
     {
-        ApplicationDbContext _context;
-
-        public EmployeeController(ApplicationDbContext context)
+        private readonly ApplicationDbContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public EmployeeController(ApplicationDbContext context, IWebHostEnvironment webHostEnvironment)
         {
             _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
         {
-            IEnumerable<Employee> employee = from e in _context.Employee select e;
-            return View();
+            IEnumerable<Employee> employees = _context.Employee.ToList();
+            
+            return View(employees);
+        }
+
+        public IActionResult Update(string? id) 
+        {
+            Employee employee = _context.Employee.Find(id);
+
+            if (employee == null) 
+            {
+                return NotFound();
+            }
+
+            EmployeeVM employeeVM = new();
+            employeeVM.Employee = employee;
+
+            // getting the list of hr managers out of db
+            string roleName = "HR manager";
+
+            var roleId = _context.Roles.FirstOrDefault(r => r.Name == roleName).Id;
+
+            var managerIds = _context.UserRoles
+                .Where(ur => ur.RoleId == roleId)
+                .Select(ur => ur.UserId)
+                .ToList();
+
+            employeeVM.HrManagerList = _context.Users
+                .Where(u => managerIds.Contains(u.Id))
+                .Select(u => new SelectListItem
+                {
+                    Text = u.FullName,
+                    Value = u.Id.ToString()
+                })
+                .ToList();
+
+            return View(employeeVM);
+        }
+
+        [HttpPost]
+        public IActionResult Update(EmployeeVM employeeVM, IFormFile? file)
+        {
+            if (ModelState.IsValid)
+            {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string photoPath = Path.Combine(wwwRootPath, @"photos");
+
+                    if (!string.IsNullOrEmpty(employeeVM.Employee.PhotoUrl))
+                    {
+                        //delete the old image
+                        var oldImagePath =
+                            Path.Combine(wwwRootPath, employeeVM.Employee.PhotoUrl.TrimStart('\\'));
+
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    using (var fileStream = new FileStream(Path.Combine(photoPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    employeeVM.Employee.PhotoUrl = @"\photos\" + fileName;
+                }
+
+                _context.Employee.Update(employeeVM.Employee);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            else
+            {
+                return View();
+            }
         }
     }
 }
